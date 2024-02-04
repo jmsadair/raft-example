46cef72aade6c0d45373775128ee75bc4aeb95ef
James Adair <jmsadair@gmail.com>
2024-02-04 14:20:56 -0500

    Update project.clj

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,7 +1,8 @@
 (defproject jepsen.raft "0.1.0-SNAPSHOT"
   :description "A Jepsen test for raft"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "http://github.com/jmsadair/raft-example"
+  :license {:name "The MIT License"
+            :url  "http://opensource.org/licenses/MIT"}
   :main jepsen.raft
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [jepsen "0.2.1-SNAPSHOT"]])
